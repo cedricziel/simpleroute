e50a33a607b25cd45a1fc77c2d6c24534efee5e2
Cedric Ziel <cedric@cedric-ziel.com>
2015-02-22 20:47:14 +0100

    [TASK] Add DirectionSearchDTO

---
+++ Tests/Unit/Domain/Model/DTO/DirectionSearchDTOTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace CedricZiel\Simpleroute\Tests\Unit\Domain\Model\DTO;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2015 Cedric Ziel <cedric@cedric-ziel.com>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use CedricZiel\Simpleroute\Domain\Model\DTO\DirectionSearchDTO;
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+
+/**
+ * Test case for class \CedricZiel\Simpleroute\Domain\Model\DTO\DirectionSearchDTO.
+ *
+ * @copyright Copyright belongs to the respective authors
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ *
+ * @author Cedric Ziel <cedric@cedric-ziel.com>
+ */
+class DirectionSearchDTOTest extends UnitTestCase {
+
+	/**
+	 * @var DirectionSearchDTO
+	 */
+	protected $subject = NULL;
+
+	protected function setUp() {
+
+		$this->subject = new DirectionSearchDTO();
+	}
+
+	protected function tearDown() {
+
+		unset($this->subject);
+	}
+
+	/**
+	 * @test
+	 */
+	public function startPropertyCanBeSet() {
+
+		$value = "Rue 1st, Halle";
+		$this->subject->setStart($value);
+		$this->assertEquals($value, $this->subject->getStart());
+	}
+
+}
